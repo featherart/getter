53367399cfb7f8f640a36568bea7d707e20f3797
Feather Knee <featherknee@gmail.com>
2018-01-07 10:00:45 -0800

    Add search component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,21 @@
-import React, { Component } from 'react';
-import People from './People';
-import './App.css';
+import React, { Component } from "react";
+import People from "./People";
+import "./App.css";
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      results: []
-    }
-  }
-
-  componentDidMount() {
-    fetch('https://randomuser.me/api/?results=25')
-      .then(results => {
-        return results.json();
-      }).then(data => {
-        console.log('data', data.results)
-        let results = data.results.map((pic) => {
-          return(
-            <div key={pic.cell}>
-              <img src={pic.picture.large} alt={pic.name.first} />
-            </div>
-          )
-        })
-        this.setState({results : results})
-      })
-  }
-
   render() {
-    const { results } = this.state
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Just Some Random People</h1>
         </header>
         <div>
+          <div className='form-container'>
+            <form>
+              <label>Search People</label>
+              <input type="text" />
+            </form>
+          </div>
           <People />
         </div>
       </div>
