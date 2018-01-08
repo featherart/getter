807eed53ceb79e4d9a20fef15b1bd881aa9106b9
Feather Knee <featherknee@gmail.com>
2018-01-07 23:38:56 -0800

    purples and pinks color theme

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
 import People from "./People";
+import TiGroupOutline from 'react-icons/lib/ti/group-outline';
+import MdSort from "react-icons/lib/md/sort";
+import MdSearch from "react-icons/lib/md/search";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Just Some Random People</h1>
+      <div className="app">
+        <header className="header">
+          <h1 className="title"><TiGroupOutline className='app-icon' />Just Some Random People</h1>
         </header>
-        <div>
-          <div className='form-container'>
+        <div className="container">
+          <div className="controls-container">
+            <button className="sort-button">Sort First Name <MdSort /></button>
+            <button className="sort-button">Sort Last Name <MdSort /></button>
             <form>
               <label>Search People</label>
               <input type="text" />
+              <button className="search-button">Search <MdSearch /></button>
             </form>
           </div>
           <People />
