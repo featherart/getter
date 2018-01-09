28b05bd3ccf2a6219a77deeea3afe291d72b4216
Feather Knee <featherknee@gmail.com>
2018-01-08 18:12:21 -0800

    Refactor colors and start to break out components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import People from "./People";
-import TiGroupOutline from "react-icons/lib/ti/group-outline";
 import MdSort from "react-icons/lib/md/sort";
 import MdSearch from "react-icons/lib/md/search";
+import Title from './Title'
 import "./App.css";
 
 class App extends Component {
@@ -21,9 +21,7 @@ class App extends Component {
     return (
       <div className="app">
         <header className="header">
-          <h1 className="title">
-            <TiGroupOutline className="app-icon" />Just Some Random People
-          </h1>
+          <Title />
         </header>
         <div className="container">
           <div className="controls-container">
