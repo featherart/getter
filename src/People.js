fc80574fe5b12d257fb9603582af08d1fab7b417
Feather Knee <featherknee@gmail.com>
2018-01-08 18:29:44 -0800

    Clean up code

--- src/People.js
+++ src/People.js
@@ -9,7 +9,7 @@ class People extends Component {
   }
 
   componentDidMount() {
-    fetch("https://randomuser.me/api/?results=25")
+    fetch("https://randomuser.me/api/?results=250")
       .then(results => {
         return results.json();
       })
@@ -26,32 +26,34 @@ class People extends Component {
       });
   }
 
+  sortBy(results, sortBy) {
+    return results.sort((a, b) => {
+      if (sortBy === "first") {
+        if (a.firstName < b.firstName) return -1;
+        if (a.firstName > b.firstName) return 1;
+        return 0;
+      } else {
+        if (a.lastName < b.lastName) return -1;
+        if (a.lastName > b.lastName) return 1;
+        return 0;
+      }
+    });
+  }
+
   render() {
     const { results } = this.state;
     const { sortBy } = this.props;
     return (
       <div className="people-container">
-        {results
-          .sort((a, b) => {
-            if (sortBy === "first") {
-              if (a.firstName < b.firstName) return -1;
-              if (a.firstName > b.firstName) return 1;
-              return 0;
-            } else {
-              if (a.lastName < b.lastName) return -1;
-              if (a.lastName > b.lastName) return 1;
-              return 0;
-            }
-          })
-          .map(pic =>
-            <div key={pic.id} className="picture-container">
-              <img src={pic.imageUrl} alt={pic.firstName} />
-              <div className="person-name">
-                {pic.firstName ? pic.firstName.toUpperCase() : ""}{" "}
-                {pic.lastName ? pic.lastName.toUpperCase() : ""}
-              </div>
+        {this.sortBy(results, sortBy).map(pic =>
+          <div key={pic.id} className="picture-container">
+            <img src={pic.imageUrl} alt={pic.firstName} />
+            <div className="person-name">
+              {pic.firstName ? pic.firstName.toUpperCase() : ""}{" "}
+              {pic.lastName ? pic.lastName.toUpperCase() : ""}
             </div>
-          )}
+          </div>
+        )}
       </div>
     );
   }
